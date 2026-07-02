b9d1b39def5bdb5c6d8cd0509e1b09e052279c22
Nitin Kanodiya <nitin.aiconsulting.india@gmail.com>
2026-07-02 21:04:03 +0530

    Fix robots sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://aiconsulting.vercel.app/sitemap.xml",
+
+    sitemap: "https://aiconsulting-iota.vercel.app/sitemap.xml",
   };
 }
