d76fa9d4ad9f2aa4555ee93fe328c931ea9fd83d
Nitin Kanodiya <nitin.aiconsulting.india@gmail.com>
2026-07-05 23:00:21 +0530

    Update robots sitemap to custom domain

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
 
-    sitemap: "https://aiconsulting-iota.vercel.app/sitemap.xml",
+    sitemap: "https://aimanufacturingconsultant.com/sitemap.xml",
   };
 }
