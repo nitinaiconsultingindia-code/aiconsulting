eebb24ffc44e73c6b46c21e159c04c48838b28e7
Nitin Kanodiya <nitin.aiconsulting.india@gmail.com>
2026-07-05 22:53:43 +0530

    Update sitemap to use custom domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://aiconsulting-iota.vercel.app";
+  const baseUrl = "https://aimanufacturingconsultant.com";
 
   return [
     {
